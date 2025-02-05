e3f33c5a9e051fed2f55e895d6f1575bd6341304
JishnuVijayan <jishnu03ptb@gmail.com>
2025-02-05 16:29:15 +0530

    Settings button hide

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -43,7 +43,7 @@ import { ThinLayoutComponent } from './layouts/vertical/thin/thin.component';
         DenseLayoutComponent,
         FuturisticLayoutComponent,
         ThinLayoutComponent,
-        SettingsComponent,
+        // SettingsComponent,
     ],
 })
 export class LayoutComponent implements OnInit, OnDestroy {
